477d58a0de7177f8bce8c69a7d442e996a3883b6
Katrine Tjoelsen <ktjolsen@mit.edu>
2016-02-27 20:39:05 -0500

    Updated logic for when registration pages are showed plus improved UI design

--- client/helpers/home.js
+++ client/helpers/home.js
@@ -4,7 +4,6 @@ Template.Home.helpers({
   ideas: function () {
     var ideas = []
     Ideas.find().forEach(function(idea) {
-      console.log(idea);
       ideas.push({
         name: idea.idea_name,
         objective: idea.objective,
@@ -18,7 +17,6 @@ Template.Home.helpers({
   problems: function () {
     var problems = []
     Problems.find().forEach(function(problem) {
-      console.log(problem);
       problems.push({
         problem_name: problem.problem_name,
         impact_area: problem.impact_area,
--- client/helpers/registeridea.js
+++ client/helpers/registeridea.js
@@ -1,4 +1,36 @@
 
+Template.RegisterIdea.helpers({
+  registeredUser: function() {
+    
+    // if user not logged in, always return false
+    if (!Meteor.userId()) {
+      return false;
+    }
+
+    Meteor.call('finishedRegistration', function(error, result){
+      Session.set('finishedRegistration', result);
+    });
+
+    if (Session.get('finishedRegistration') == true) {
+      return true
+    }
+    else {
+      return false;
+    }
+  }
+});
+
+
+Template.RegisterIdea.events({
+    'click #finish-user-registration-btn': function() {
+        window.location.href = '/userinfo';
+    },
+
+    'click #go-to-login-btn': function() {
+        window.location.href = '/login';
+    }
+});
+
 Template.registerIdeaForm.helpers({
     is_business_project: function() {
         var objective = AutoForm.getFieldValue("objective", "registerIdeaForm");
